55cca2bf4422bb0d90e381a3cb09af72c40d3005
Zobayada Afnan <azobayada@gmail.com>
2023-05-01 02:39:30 +0600

    first

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import "./navbar.scss";
+import {SiYourtraveldottv} from "react-icons/si";
+import {AiFillCloseCircle} from "react-icons/ai";
+import {TbGridDots} from "react-icons/tb";
+
+const Navbar = () => {
+  return (
+    <section className='navBarSection'>
+      <header className='header flex'>
+
+        <div className='logoDiv'>
+          <a href='#' className='logo flex'>
+            <h1> <SiYourtraveldottv className="icon"/> Travel.</h1>
+          </a>
+        </div>
+
+        <div className='navBar'>
+          <ul className='navList flex'>
+            <li className='navItem'>
+              <a href="#" className="navLink">Home</a>
+            </li>
+            <li className='navItem'>
+              <a href="#" className="navLink">Packages</a>
+            </li>
+            <li className='navItem'>
+              <a href="#" className="navLink">About</a>
+            </li>
+            <li className='navItem'>
+              <a href="#" className="navLink">Shop</a>
+            </li>
+            <li className='navItem'>
+              <a href="#" className="navLink">Pages</a>
+            </li>
+            <li className='navItem'>
+              <a href="#" className="navLink">Contact</a>
+            </li>
+
+            <button className='btn'>
+              <a href="#"> Book Now</a>
+            </button>
+          </ul>
+
+          <div className="closeNavbar">
+            <AiFillCloseCircle className='icon'/>
+          </div>
+        </div>
+
+        <div className="toggleNavbar">
+          <TbGridDots className='icon'/>
+        </div>
+      </header>
+    </section>
+  )
+}
+
+export default Navbar
