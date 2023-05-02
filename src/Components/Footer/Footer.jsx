4f8b51a12ef6d17a48115332c4a933c2983d6327
Zobayada Afnan <azobayada@gmail.com>
2023-05-02 23:44:49 +0600

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
-import "./footer.css";
+import "./footer.scss";
+import video2 from "../../Assets/video2.mp4";
+import {FiSend} from "react-icons/fi";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+    <section className="footer">
+      <div className="videoDiv">
+        <video src={video2} muted autoPlay loop typeof="video/mp4"></video>
+      </div>
+
+      <div className="secContent container">
+        <div className="contactDiv flex">
+          <div className="text">
+            <small>KEEP IN TOUCH</small>
+            <h2>Travel With Us</h2>
+          </div>
+
+          <div className="inputDiv flex">
+            <input type="text" placeholder='Enter Email Address'/>
+            <button className='btn flex' type='submit'>
+             Send <FiSend className='icon'/>
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Footer
